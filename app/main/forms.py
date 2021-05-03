b87f09033e5e92f2b0f8df41fe1053518b9d8372
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-03 18:08:29 +0300

    Add classes for blog and comment forms.

--- app/main/forms.py
+++ app/main/forms.py
@@ -10,3 +10,22 @@ class UpdateProfile(FlaskForm):
 
     bio = TextAreaField('Tell us about you.', validators=[Required()])
     submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    """
+    Class for providing the blog.
+    """
+
+    username = StringField("Please enter your username.", validators=[Required()])
+    title = StringField('Blog Title',validators=[Required()])
+    description = StringField('Description',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    """
+    Class for provinding comments for the blog.
+    """
+
+    username = StringField("Please enter your username.", validators=[Required()])
+    comment = TextAreaField('Write a comment', validators=[Required()])
+    submit = SubmitField('Submit')
